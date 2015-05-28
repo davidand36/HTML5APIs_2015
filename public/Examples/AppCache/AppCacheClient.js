aa142e71b363e69227aab1bb946748edcc2df76e
David M. Anderson <dma@epsilondelta.us>
2015-05-28 09:31:47 -0700

    Added call to update application cache and watch for update event

--- public/Examples/AppCache/AppCacheClient.js
+++ public/Examples/AppCache/AppCacheClient.js
@@ -15,5 +15,16 @@ var text = 'The time on this computer is ' + clientTime.toString() + '<br />' +
         serverTimeString;
 $('main').html( text );
 
+//-----------------------------------------------------------------------------
+
+window.applicationCache.update( );
+
+window.applicationCache.addEventListener(
+    'updateready',
+    function( ) {
+        window.applicationCache.swapCache( );
+        window.location.reload( );
+    } );
+
 //=============================================================================
 })();
