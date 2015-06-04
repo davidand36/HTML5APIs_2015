9cf30ddba88bea6570010dbd488875dff21d3204
David M. Anderson <dma@epsilondelta.us>
2015-06-04 07:09:40 -0700

    Added logging of app cache events. Disabled cache swap and page reload

--- public/Examples/AppCache/AppCacheClient.js
+++ public/Examples/AppCache/AppCacheClient.js
@@ -10,21 +10,47 @@
 
 //=============================================================================
 
+
 var clientTime = new Date();
 var text = 'The time on this computer is ' + clientTime.toString() + '<br />' +
         serverTimeString;
 $('main').html( text );
 
-//-----------------------------------------------------------------------------
 
-window.applicationCache.update( );
+//=============================================================================
 
-window.applicationCache.addEventListener(
-    'updateready',
-    function( ) {
-        window.applicationCache.swapCache( );
-        window.location.reload( );
-    } );
+
+var cache = window.applicationCache;
+var cacheEvents = [ 'cached', 'checking', 'downloading', 'error',
+                    'noupdate', 'obsolete', 'progress', 'updateready' ];
+var cacheStatuses = [ 'uncached', 'idle', 'checking', 'downloading',
+                      'updateready', 'obsolete' ];
+var i, len;
+
+if ( cache ) {
+    for ( i = 0, len = cacheEvents.length; i < len; ++i ) {
+        cache.addEventListener( cacheEvents[ i ], logCacheEvent, false );
+    }
+
+    //cache.update( );
+    /*
+    cache.addEventListener(
+        'updateready',
+        function( ) {
+            window.applicationCache.swapCache( );
+            window.location.reload( );
+        },
+        false );
+    */
+}
+
+function logCacheEvent( event ) {
+    var msg = 'Online: ' + (navigator.onLine ? 'yes' : 'no') +
+        '  Cache status: ' + cacheStatuses[ cache.status ] +
+        '  Event type: ' + event.type;
+    console.log( msg );
+}
+
 
 //=============================================================================
 })();
